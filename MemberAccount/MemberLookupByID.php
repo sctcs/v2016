b4bf14a9aa2301e547aefe5b80d936162fec0a81
Pinxia Li <pinxia820@gmail.com>
2016-12-17 16:21:56 -0500

    added a link to see other members of family after see one record

--- MemberAccount/MemberLookupByID.php
+++ MemberAccount/MemberLookupByID.php
@@ -21,6 +21,7 @@ include("../common/DB/DataStore.php");
 ////  1. load all variables ////
 
 
+
 if (isset($_POST[memberID]) || isset($_POST[familyID])) {
     $memberID = trim($_POST[memberID]);
     $familyID = trim($_POST[familyID]);
@@ -39,17 +40,12 @@ if (($memberID == "") && ($familyID == "") && ($memid == "")) {
     exit();
 }
 
-
 if (isset($_SESSION['logon'])) {
     $seclvl = $_SESSION['membertype'];
     $secdesc = $_SESSION['MemberTypes'][$seclvl];
 }
 
-
-$SQLstring = 'SELECT `FamilyID`, `MemberID`, `FirstName`, `LastName`, `ChineseName`, `HomePhone`, `OfficePhone`, `CellPhone`, `Email`, `SecondEmail`, `HomeAddress`, `HomeCity`, `HomeState`, `HomeZip`, `Profession`,`UserName`,`Password`,`PrimaryContact` FROM `tblMember`';
-
-$SQLstring = 'SELECT `FamilyID`, `MemberID`, `FirstName`, `LastName`, `ChineseName`, `HomePhone`, `OfficePhone`, `CellPhone`, `Email`, `SecondEmail`, `HomeAddress`, `HomeCity`, `HomeState`, `HomeZip`, `Profession`,`UserName`,`Password` FROM `tblMember`';
-
+$SQLstring = 'SELECT `FamilyID`, `MemberID`,`UserName`, `Password`, `FirstName`, `LastName`, `ChineseName`, `HomePhone`, `OfficePhone`, `CellPhone`, `Email`, `SecondEmail`, `HomeAddress`, `HomeCity`, `HomeState`, `HomeZip`, `Profession`,`UserName`,`Password`,`PrimaryContact` FROM `tblMember`';
 
 
 if ($memberID != "") {
@@ -75,7 +71,7 @@ $RS1 = mysqli_query($conn, $SQLstring);
 $rc = 0;
 while ($row1 = mysqli_fetch_array($RS1)) {
     $mid = $row1[MemberID];
-    $lname = $row1[LastName];
+    //  $lname = $row1[LastName];
     $rc = $rc + 1;
     $rows[$rc] = $row1;
 }
@@ -96,170 +92,149 @@ while ($row1 = mysqli_fetch_array($RS1)) {
             ?>
             <h3>Member Record</h3>
             <table border="1" class="table table-striped">
-
-                <?php if (isset($_SESSION['logon']) && ( $seclvl <= 25 || $seclvl == 35 || $seclvl == 40 || $seclvl == 55)) {
+                <?php if(isset($_SESSION['logon']) && ( $seclvl <= 25 || $seclvl === 35 || $seclvl === 40 || $seclvl === 55)) {
                     ?>
-
-                    <tr><td>Member ID:</td>
-                        <td><?php echo $row[MemberID]; ?></td>
-                    </tr>
-                    <tr><td>Family ID:</td>
-                        <td><?php echo $row[FamilyID]; ?></td>
-                    </tr>
-
-                    <?php
-                    if (isset($SESSION['logon']) && $seclvl == 20) {
-                        ?>
-                        <tr><td>Login:</td>
-                            <td><?php echo $row[UserName]; ?></td>
-                        </tr>
-                        <tr><td>Password:</td>
-                            <td><?php echo $row[Password]; ?></td>
-                        </tr>
-                        <?php
-                    }
-                }
-                ?>
-
-                <tr><td>Last Name:</td>
-                    <td><?php echo $row[LastName]; ?></td>
-                </tr>
-                <tr><td>First Name:</td>
-                    <td><?php echo $row[FirstName]; ?></td>
-                </tr>
-                <tr><td>Chinese Name:</td>
-                    <td><?php echo $row[ChineseName]; ?></td>
-                </tr>
-                <?php if (isset($_SESSION['logon'])) { ?>
-                    <tr><td>Home Phone:</td>
-                        <td><?php echo $row[HomePhone]; ?></td>
-                    </tr>
-                    <?php if ($seclvl <= 25 || $seclvl == 35 || $seclvl == 40 || $seclvl == 55) { ?>
-                        <tr><td>Email:</td>
-                            <td><?php echo $row[Email]; ?></td>
-                        </tr>
-                        <tr><td>Office Phone:</td>
-                            <td><?php echo $row[OfficePhone]; ?></td>
-                        </tr>
-                        <tr><td>Cell Phone:</td>
-                            <td><?php echo $row[CellPhone]; ?></td>
-                        </tr>
-                        <tr><td>Street:</td>
-                            <td><?php echo $row[HomeAddress]; ?></td>
-                        </tr>
-                        <tr><td>City:</td>
-                            <td><?php echo $row[HomeCity]; ?></td>
-                        </tr>
-                        <tr><td>State:</td>
-                            <td><?php echo $row[HomeState]; ?></td>
-                        </tr>
-                        <tr><td>Zip:</td>
-                            <td><?php echo $row[HomeZip]; ?></td>
-                        </tr>
-                        <tr><td>Profession:</td>
-                            <td><?php echo $row[Profession]; ?></td>
-                        </tr>
-
-                        <?php
-                    }
-                }
-            }
-
-
-        if ($rc > 1) {
-                ?>
-                <h3>Member Records</h3>
-                <table border="0" class="table table-striped">
-                    <tr><th>Member List</th><th>English Name</th><th>Chinese Name</th><th>Member ID</th><th>Primary Contact</th></tr>
-
-            if ($rc > 1) {
-                ?>
-                <h3>Member Records</h3>
-                <table border="0" class="table table-striped">
-                    <tr><th>Member List</th><th>English Name</th><th>Chinese Name</th><th>Member ID</th></tr>
-
-                    <?php
-                    //   echo "see 2: ".$SQLstring;
-                    $RS1 = mysqli_query($conn, $SQLstring);
-                    $rc = 0;
-
-                    while ($row = mysqli_fetch_array($RS1)) {
-                        $rc = $rc + 1;
+                    <p class="text-center"><a href="MemberLookupByID.php?fid=<?php echo $row[FamilyID] . "\">" . "More Members in FamilyID#='" . $row[FamilyID] . "' && LastName='" . $row[LastName] . "'</a>"; ?><?php $row[LastName] . " " . $row[FamilyID]; ?></p>
+                            <tr><td>Member ID:</td>
+                            <td><?php echo $row[MemberID]; ?></td>
+                            </tr>
+                            <tr><td>Family ID:</td>
+                            <td><?php echo $row[FamilyID]; ?></td>
+                            </tr>
+                       <?php   
+                        }
                         ?>
 
-                    
-                        <tr><td><?php echo $rc; ?>.&nbsp</td><td>
-                           <a href="MemberLookupByID.php?memid=<?php echo $row[MemberID] . "\">" . $row[LastName] . ", " . $row[FirstName]  ."</a></td><td>";?><?php echo $row[ChineseName] . "</td><td> " . $row[MemberID]. "</td><td>".$row[PrimaryContact]."</td>"; ?></td>
-                              <?php if ($row[PrimaryContact]=="Yes") {
-                                  echo "<h4>Family ID#: ".$row[FamilyID]."</h4>";
-                              }
-                            ?>
+                        <tr><td>Last Name:</td>
+                        <td><?php echo $row[LastName]; ?></td>
                         </tr>
-                        
+                        <tr><td>First Name:</td>
+                        <td><?php echo $row[FirstName]; ?></td>
+                        </tr>
+                        <tr><td>Chinese Name:</td>
+                        <td><?php echo $row[ChineseName]; ?></td>
+                        </tr>
+                        <?php if (isset($_SESSION['logon'])) { ?>
+                            <tr><td>Home Phone:</td>
+                            <td><?php echo $row[HomePhone]; ?></td>
+                            </tr>
+                            
+                           <?php if($seclvl == 20) { ?>
+                          <tr><td>Login:</td>
+                                <td><?php echo $row[UserName]; ?></td>
+                                </tr>
+                                <tr><td>Password:</td>
+                                <td><?php echo $row[Password]; ?></td>
+                                </tr>
                          
+                        <?php }
+                            
+                            if ($seclvl <= 25 || $seclvl == 35 || $seclvl == 40 || $seclvl == 55) { ?>
+                                <tr><td>Email:</td>
+                                <td><?php echo $row[Email]; ?></td>
+                                </tr>
+                                <tr><td>Office Phone:</td>
+                                <td><?php echo $row[OfficePhone]; ?></td>
+                                </tr>
+                                <tr><td>Cell Phone:</td>
+                                <td><?php echo $row[CellPhone]; ?></td>
+                                </tr>
+                                <tr><td>Street:</td>
+                                <td><?php echo $row[HomeAddress]; ?></td>
+                                </tr>
+                                <tr><td>City:</td>
+                                <td><?php echo $row[HomeCity]; ?></td>
+                                </tr>
+                                <tr><td>State:</td>
+                                <td><?php echo $row[HomeState]; ?></td>
+                                </tr>
+                                <tr><td>Zip:</td>
+                                <td><?php echo $row[HomeZip]; ?></td>
+                                </tr>
+                                <tr><td>Profession:</td>
+                                <td><?php echo $row[Profession]; ?></td>
+                                </tr>
 
-                        <tr><td><?php echo $rc; ?>.&nbsp</td><td>
-                                   <a href="MemberLookupByID.php?memid=<?php echo $row[MemberID] . "\">" . $row[LastName] . ", " . $row[FirstName]  ."</a></td><td>";?><?php echo $row[ChineseName] . "</td><td> " . $row[MemberID]; ?></td>
+                                <?php
+                            }
+                        }
+                                  ?>
+                          </table>  
+                     <?php }  //end $rc=1
 
-                        </tr>
-                                   <?php
+             if ($rc > 1) {   ?>
+                   <h3>Member Records</h3>
+                   <table border="1" class="table table-striped">
+                        <tr><th>Member List</th><th>English Name</th><th>Chinese Name</th><th>Member ID</th><th>Primary Contact</th></tr>
+                        <?php
+                        // var_dump($rows);
 
-                               }
-                           } //$rc > 1 
-                           else if ($rc == 1) {
-                               //header( 'Location: MemberLookupByID.php?memid=' .$mid);
-                               //$seclvl = $_SESSION['membertype'];
-                               if (isset($_SESSION['logon']) && ($seclvl <= 25 || $seclvl == 35 || $seclvl == 40 || $seclvl == 55)) {
+                        for ($i = 1; $i <= $rc; $i++) {
+                            ?>
+                            <tr><td><?php echo $i; ?>.&nbsp</td><td>
+                                    <a href="MemberLookupByID.php?memid=<?php echo $rows[$i][MemberID] . "\">" . $rows[$i][LastName] . ", " . $rows[$i][FirstName] . "</a></td><td>"; ?><?php echo $rows[$i][ChineseName] . "</td><td> " . $rows[$i][MemberID] . "</td><td>" . $rows[$i][PrimaryContact] . "</td>"; ?></td>
+                                    <?php
+                                    if ($rows[$i][PrimaryContact] == "Yes") {
+                                        echo "<h4>Family ID#: " . $rows[$i][FamilyID] . "</h4>";
+                         }
+                 }   ?>
+                    </table>
+                 <?php
+                 } //end $rc>1
+                
+                else if ($rc == 1) {
+                //header( 'Location: MemberLookupByID.php?memid=' .$mid);
+                //$seclvl = $_SESSION['membertype'];
+                if (isset($_SESSION['logon']) && ($seclvl <= 25 || $seclvl == 35 || $seclvl == 40 || $seclvl == 55)) {
 
-                                   if ($memid != "") {
-                                       ?>
-                                       </table> </div>
-                                       <div class='col-sm-12'>
-                                       <h3>Detailed Record of <?php echo $row[FirstName] . '  ' . $row[LastName]; ?></h3>
-                                       <table border=1 class='table table-striped'>
-                                       <tr><th>ClassID</th><th>ClassRegID</th><th>Grade</th><th>Class</th><th>Classroom</th><th>Year</th><th>Term</th><th>Current<br>Class</th><th>Tuition</th></tr>
-                                       <?php
-                                       //      echo $SQLclass;
-                                       while ($rowc = mysqli_fetch_array($RSc)) {
-                                           ?>
-                                           <tr>
-                                           <td><?php echo $rowc[ClassID]; ?></td>
-                                           <td><?php echo $rowc[ClassRegistrationID]; ?></td>
-                                           <td><?php echo $rowc[GradeOrSubject]; ?></td>
-                                           <td><?php echo $rowc[ClassNumber]; ?></td>
-                                           <td><?php echo $rowc[Classroom]; ?></td>
-                                           <td><?php echo $rowc[Year]; ?></td>
-                                           <td><?php echo $rowc[Term]; ?></td>
-                                           <td><?php echo $rowc[CurrentClass]; ?></td>
-                                           <td><?php echo $rowc[ClassFee]; ?></td>
-                                           </tr>
-                                           <?php
-                                       }
-                                   }
-                               }
-                           } else {  //no record found
-                               if ($memid == "") {
-                                   echo "<p class='red-color text-center'>no record found for " . $memid . ", <a href=\"MemberLookupByIDForm.php\">continue ...</a>";
-                                   exit;
-                               }
-                           }
-                           mysqli_close($conn);
+                    if ($memid != "") { ?>
+                        </div>
+                        <div class='col-sm-12'>
+                        <h3>Detailed Record of <?php echo $row[FirstName] . '  ' . $row[LastName]; ?></h3>
+                        <table border=1 class='table table-striped'>
+                        <tr><th>ClassID</th><th>ClassRegID</th><th>Grade</th><th>Class</th><th>Classroom</th><th>Year</th><th>Term</th><th>Current<br>Class</th><th>Tuition</th></tr>
+                    <?php
+           
+                        while ($rowc = mysqli_fetch_array($RSc)) {
                            ?>
-                           </table>
-                           <p>
-                           <a href="MemberLookupByID.php?
-                           <?php if ($familyID != "") { ?>
-
-                               fid=<?php echo $familyID; ?>">View list of Family ID=<?php echo $familyID; ?> ||
-
-                               fid=<?php echo $familyID; ?>">View list of <?php echo $familyID; ?> ||
-
-                           <?php } else if ($memberID != "") { ?> 
-                               memid=<?php echo $memberID; ?>">Details of Member ID='<?php echo $memberID; ?>' 
-                           <?php } ?></a> ||
-
-                        &nbsp;&nbsp;<a href="MemberLookupByIDForm.php">Lookup By ID</a>&nbsp;||&nbsp;<a href='MemberLookupForm.php'>Look up By Name</a>
-
-                        </div>
-                        </div>
-                        <?php include("../common/site-footer1.php"); ?>
+                            <tr>
+                            <td><?php echo $rowc[ClassID]; ?></td>
+                            <td><?php echo $rowc[ClassRegistrationID]; ?></td>
+                            <td><?php echo $rowc[GradeOrSubject]; ?></td>
+                            <td><?php echo $rowc[ClassNumber]; ?></td>
+                            <td><?php echo $rowc[Classroom]; ?></td>
+                            <td><?php echo $rowc[Year]; ?></td>
+                            <td><?php echo $rowc[Term]; ?></td>
+                            <td><?php echo $rowc[CurrentClass]; ?></td>
+                            <td><?php echo $rowc[ClassFee]; ?></td>
+                            </tr>
+                            <?php
+                        }
+                    }?>
+                       </table>
+                  </div>
+                       <?php }
+                    }    //End $rc=1
+                    else {  //No record found
+                        if ($memid == "") {
+                            echo "<h3 class='text-center'>Member not exist</h3><p class='red-color text-center'>Member ID does not exist, <a href=\"MemberLookupByIDForm.php\">search again ...</a>";
+                            exit;
+                        }
+                    }
+                    mysqli_close($conn);
+                    ?>
+                    </table>
+        
+      <div class='container'> 
+            <a href="MemberLookupByID.php?
+            <?php if ($familyID != '') { ?>
+                  fid=<?php echo $familyID; ?>">Current Family ID=<?php echo $familyID; ?> 
+            <?php } else if ($memberID != '') { ?> 
+                  memid=<?php echo $memberID; ?>">Details of Member ID='<?php echo $memberID; ?>' 
+            <?php } ?></a> || &nbsp;&nbsp;<a href="MemberLookupByIDForm.php">Lookup By ID</a>&nbsp;
+                         ||&nbsp;<a href='MemberLookupForm.php'>Look up By Name</a>
+      </div>
+          <br/>
+         </div>
+    </div>
+<?php include("../common/site-footer1.php"); ?>
