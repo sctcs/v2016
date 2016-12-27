f969a04c73672ff6563f8ad2f4f6fc0dc493682d
Pinxia Li <pinxia820@gmail.com>
2016-12-26 23:58:22 -0500

    #22

--- MemberAccount/UpdateMemberProfile.php
+++ MemberAccount/UpdateMemberProfile.php
@@ -4,9 +4,20 @@ if ( $_SERVER["SERVER_NAME"] != "localhost" ) {
   session_save_path("/home/users/web/b2271/sl.ynhchine/phpsessions");
 }
 session_start();
-if(!isset($_SESSION['family_id']) && !isset($_POST[newmember]) )
+
+if (!isset($_SESSION['logon']) || $seclvl > 25 && $seclvl != 40) 
+ {  
+    echo ("You cannot update member") ;
+    exit();
+ }
+
+
+$mid = $_POST["mid"];
+//echo "member id: ". $mid;
+
+if(!isset($mid) || $mid =="")
 {
- header( 'Location: ../main.php' ) ;
+ header( 'Location: MemberAccountMain.php' ) ;
  exit();
 }
 include("../common/DB/DataStore.php");
@@ -17,35 +28,29 @@ include("../common/CommonParam/params.php");
 ////////////////////////////////////////////
 // 1. get all variables
 // 2. insert into DB
-// 3. Send email out
-// 4. create necessary session
-// 5. redirect page to new location
+// 3. redirect page to new location
 ///////////////////////////////////////////
 
 
 ////////  1. load all variables/////////////////////
 
 
-$PrimaryCP_FirstName=$_POST[PCFristName];
-$PrimaryCP_FirstName=str_replace("'", "''",$PrimaryCP_FirstName);
-$PrimaryCP_FirstName=str_replace("\n", " ",$PrimaryCP_FirstName);
-$PrimaryCP_FirstName=trim($PrimaryCP_FirstName);
+$FirstName=$_POST[PCFristName];
+$FirstName=str_replace("'", "''",$FirstName);
+$FirstName=str_replace("\n", " ",$FirstName);
+$FirstName=trim($FirstName);
 
-$PrimaryCP_LastName=$_POST[PCLastName];
+$LastName=trim($_POST[PCLastName]);
 //$LoginPW=$_POST[PW];
-$PrimaryCP_ChineseName=$_POST[PCChineseName];
+$ChineseName=$_POST[PCChineseName];
 
-$PrimaryCP_Profession=$_POST[PCProfession];
+$email=$_POST[PCEmail];
+$email=str_replace("'", "''",$email);
+$email=str_replace("\n", " ",$email);
+$email=trim($email);
 
-$PrimaryCP_email=$_POST[PCEmail];
-$PrimaryCP_email=str_replace("'", "''",$PrimaryCP_email);
-$PrimaryCP_email=str_replace("\n", " ",$PrimaryCP_email);
-$PrimaryCP_email=trim($PrimaryCP_email);
+$secondemail=$_POST[SCEmail];
 
-$SecondaryCP_FirstName=$_POST[SCFristName];
-$SecondaryCP_LastName=$_POST[SCLastName];
-$SecondaryCP_email=$_POST[SCEmail];
-$SecondaryCP_Profession=$_POST[SCProfession];
 $area=$_POST[area];
 $prefix=$_POST[prefix];
 $suffix=$_POST[suffix];
@@ -68,108 +73,26 @@ $zip=$_POST[zip];
 
 $PrimaryContact=$_POST[PrimaryContact];
 $gender=$_POST[gender];
-$org=$_POST[org];
-$hobbies=$_POST[hobbies];
-$secondemail=$_POST[SecondEmail];
-if ( $_POST[dir] == "no dir" ) {
-   $is_dir_ok="No";
-}
-if ( $_POST[pic] == "no pic" ) {
-   $is_pic_ok="No";
-}
-
-if ( $_POST[password1] != "") {
- if (  $_POST[password1] == $_POST[password2] ) {
-  $pw = $_POST[password1];
- } else {
-  die ("New Passwords do not match, try again");
- }
-}
-
-$ethnicity=$_POST[ethnicity];
-$yob=$_POST[yob];
-$dobbs=$_POST[dobbs];
-//if (isset($dob) && strlen($dob) == 10) {
-//   list($mm,$dd,$yyyy) = explode("/",$dob);
-//   $dob=$yyyy."-".$mm."-".$dd;
-//}
-if ( $_POST[whose]=="student" ) {
- if ( isset($yob) ) {
-  if ( strlen($yob) == 4) {
-   //list($mm,$dd,$yyyy) = explode("/",$dob);
-  // $dob=$yyyy."-".$mm."-".$dd;
-  //list($yyyy,$mm,$dd) = explode("-",$dob);
-
-   // Convert dates to Julian Days
-   //$start_date = gregoriantojd($mm, $dd, $yyyy);
-   //$end_date = gregoriantojd(12, 31, ($CurrentYear - $AGE_TO_START));
-
-   // Return difference
-   $age = $CurrentYear - $yob;
-   if ( $age < $AGE_TO_START || ($age == $AGE_TO_START && $dobbs != 'Y') ) {
-     echo "yob: ".$yob;
-     echo "age ".$age;
-     echo "Age to start: ".$AGE_TO_START;
-     echo "Sorry! Your child is too young for school this year. Must be born on or before September 1, ". ($CurrentYear - 6). " to qualify. Please come back next year.";
-     mysqli_close($conn);
-     exit;
-   }
-
-  } else {
-   echo "ERROR: please enter a valid Year of Birth (YOB) in the format of yyyy";
-   mysqli_close($conn);
-   exit;
-  }
- } else {
-   echo "ERROR: please enter a valid Year of Birth (YOB) in the format of yyyy";
-   mysqli_close($conn);
-   exit;
-
- }
-}
 
 //echo "<br>address:  ".$address;
 
 //$MemberRegistrationDate=date("j, n, Y");
 $MemberUpdateDate=date("Y/m/d");
 
-if ( isset($_POST[newmember]) )
-{
- //////// 2. insert a record in member table ///////////////
 
- $SQLstring ="INSERT INTO member (HomePhone,FirstName,LastName,ChineseName,Email,Profession,CellPhone,HomeAddress,HomeCity,HomeState, HomeZip,"
-            ."PrimaryContact,SecondEmail,OfficePhone,Organization,Hobbies,Directory,Picture, MostRecentUpdateDate) VALUES ("
-            .$MemberType.",'". $home_phone ."','". $PrimaryCP_FirstName ."','". $PrimaryCP_LastName ."','". $PrimaryCP_email ."','"
-            .$LoginPW ."','". $PrimaryCP_Profession ."','". $SecondaryCP_FirstName ."','". $SecondaryCP_LastName ."','".  $SecondaryCP_email ."','"
-            . $SecondaryCP_Profession ."','". $cell_phone ."','". $address ."','". $city ."','". $zip ."',". $disclaimer .",". $is_dir_ok .","
-            . $is_pic_ok .",". $volunteer .",'". $volunteer_other  ."','". $MemberRegistrationDate ."') ";
-
- //echo "<br>SQLstring:  ".$SQLstring;
- //mysqli_query($conn,$SQLstring);
-} else {
  ////////  update a record in member table ////////////////////
 
 
 
- $SQLstring = "update tblMember set HomePhone='". $home_phone
-           . "', ChineseName='". $PrimaryCP_ChineseName ."', Email='". $PrimaryCP_email  ."', Profession='". $PrimaryCP_Profession  ."', CellPhone='". $cell_phone ."', HomeAddress='". $address
+ $SQLstring = "update tblMember set ChineseName='". $ChineseName. "', Gender='". $gender
+           . "', HomePhone='". $home_phone ."', Email='". $email  . "', CellPhone='". $cell_phone ."', HomeAddress='". $address
            . "', HomeCity='". $city . "', HomeState='". $state  ."', HomeZip='". $zip  ."', MostRecentUpdateDate='". $MemberUpdateDate
-           . "', Gender='". $gender. "', Ethnicity='". $ethnicity
+           . "', PrimaryContact='". $PrimaryContact
            . "', SecondEmail='". $secondemail
-           . "', OfficePhone='". $office_phone
-           . "', Organization='". $org
-           . "', Hobbies='". $hobbies
-           . "', Directory='". $is_dir_ok
-           . "', Picture='". $is_pic_ok;
- if ( strlen($yob) == 4 ) {
-   $SQLstring .=  "', DOBYear='". $yob;
-   $SQLstring .=  "', DOBBeforeSept='". $dobbs;
- }
- if ( $pw != "" ) {
-   $SQLstring .=  "', Password='". $pw;
- }
- $SQLstring .= "'  where MemberID=".$_POST['updtmemberid'];
-} // if newmember
+           . "', OfficePhone='". $office_phone ;
+ 
+ $SQLstring .= "'  where MemberID=".$mid;
+
 
 //echo "<br>SQLstring:  ".$SQLstring;
 
@@ -177,29 +100,13 @@ if (!mysqli_query($conn,$SQLstring))
   {
   die('Error: ' . mysqli_error($conn));
   }
-
-
-
-if ( $_POST[whose]=="student" ) {
-  $sql = "update tblStudent set PrimaryHomeLanguage='".$_POST[PrimaryHomeLanguage] . "', NumChSpeakParents=".$_POST[NumChSpeakParents]. " where MemberID=".$_POST['updtmemberid'];
-  //echo $sql;
-  if (!mysqli_query($conn,$sql))
-  {
-    die('Error: ' . mysqli_error($conn));
-  }
-
-echo "<center><br><br>";
-
-echo "Profile updated successfully, click here to <br><a href=\"studentProfile.php?whose=student&stuid=".$_POST['updtmemberid']."\">continue</a>";
-echo "<br>or  <br><a href=\"FamilyChildList.php\">Child List</a>";
-echo "</center>";
-} else {
-echo "<center><br><br>";
-echo "Profile updated successfully, click here to <a href=\"MemberAccountMain.php\">continue</a>";
-echo "</center>";
-
-}
-
+?>
+<div class="container" style="background: #D9D9D9; height: 300px; width: 400px;">
+    <h3>Profile Updated Successfully</h3>
+    <p class="text-center lead"><a href="MemberLookupByID.php?memid=<?php echo $mid ;?>">View Member Detail</a><p>
+    <p class="text-center lead"> <a href="MemberAccountMain.php">Go To Main Account</a></p>
+</div>
+<?php
 mysqli_close($conn);
 
 ?>
